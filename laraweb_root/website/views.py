f06ebd6a5a1ec791df4553c036502d3f37b0a3f6
shailstar <shailstar201595@gmail.com>
2021-05-04 22:12:20 +0530

    basic setup for lara-makeup website

---
+++ laraweb_root/website/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'website/home.html')
+
+
+def about(request):
+    return render(request, 'website/about.html')
+
+
+def services(request):
+    return render(request, 'website/services.html')
+
+
+def blog(request):
+    return render(request, 'website/blog.html')
+
+
+def portfolio(request):
+    return render(request, 'website/portfolio.html')
+
+
+def contact(request):
+    return render(request, 'website/contact.html')
